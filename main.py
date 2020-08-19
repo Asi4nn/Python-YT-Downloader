4e92b48de39f414298e9ce9b23bc00cfb202328d
Leo Wang <leo.wang142001@gmail.com>
2020-08-19 12:51:16 -0400

    problems with playlist and some videos

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ except:
 
 from pytube import YouTube
 from pytube import Playlist
+import pytube
 
 
 def convert_time(seconds: int):
@@ -52,36 +53,59 @@ def convert_time(seconds: int):
 #####################################
 # EDIT THESE
 #####################################
-download_dir = 'C:\Users\leowa\Music'
+download_dir = 'C:\\Users\\leowa\\Music'
 audio_itag = 140  # streams have a specific identifying tag, 140 is 128kbps audio only
 
+'''
+These are the common stream formats:
+Only change the itag if you know what you're doing
 
-inUse = True
+<Stream: itag="18" mime_type="video/mp4" res="360p" fps="30fps" vcodec="avc1.42001E" acodec="mp4a.40.2" progressive="True" type="video">
+<Stream: itag="22" mime_type="video/mp4" res="720p" fps="30fps" vcodec="avc1.64001F" acodec="mp4a.40.2" progressive="True" type="video">
+<Stream: itag="137" mime_type="video/mp4" res="1080p" fps="30fps" vcodec="avc1.640028" progressive="False" type="video">
+<Stream: itag="248" mime_type="video/webm" res="1080p" fps="30fps" vcodec="vp9" progressive="False" type="video">
+<Stream: itag="136" mime_type="video/mp4" res="720p" fps="30fps" vcodec="avc1.64001f" progressive="False" type="video">
+<Stream: itag="247" mime_type="video/webm" res="720p" fps="30fps" vcodec="vp9" progressive="False" type="video">
+<Stream: itag="135" mime_type="video/mp4" res="480p" fps="30fps" vcodec="avc1.4d401e" progressive="False" type="video">
+<Stream: itag="244" mime_type="video/webm" res="480p" fps="30fps" vcodec="vp9" progressive="False" type="video">
+<Stream: itag="134" mime_type="video/mp4" res="360p" fps="30fps" vcodec="avc1.4d401e" progressive="False" type="video">
+<Stream: itag="243" mime_type="video/webm" res="360p" fps="30fps" vcodec="vp9" progressive="False" type="video">
+<Stream: itag="133" mime_type="video/mp4" res="240p" fps="30fps" vcodec="avc1.4d4015" progressive="False" type="video">
+<Stream: itag="242" mime_type="video/webm" res="240p" fps="30fps" vcodec="vp9" progressive="False" type="video">
+<Stream: itag="160" mime_type="video/mp4" res="144p" fps="30fps" vcodec="avc1.4d400c" progressive="False" type="video">
+<Stream: itag="278" mime_type="video/webm" res="144p" fps="30fps" vcodec="vp9" progressive="False" type="video">
+<Stream: itag="140" mime_type="audio/mp4" abr="128kbps" acodec="mp4a.40.2" progressive="False" type="audio">
+<Stream: itag="249" mime_type="audio/webm" abr="50kbps" acodec="opus" progressive="False" type="audio">
+<Stream: itag="250" mime_type="audio/webm" abr="70kbps" acodec="opus" progressive="False" type="audio">
+<Stream: itag="251" mime_type="audio/webm" abr="160kbps" acodec="opus" progressive="False" type="audio">
+'''
 
-while inUse:
+while True:
     url = input('Enter the url of the video or playlist: ')
-    '''  playlists dont work
-    if 'list' in url:
-        video = []
-        playlist = Playlist(url)
-        playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
-        for v in playlist.video_urls:
-            video.append(YouTube(v))
-    else:
-    '''
+    print()
     video = None
+
     try:
-        video = YouTube(url)
+        if 'list' in url:
+            video = []
+            playlist = Playlist(url)
+            playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+            print(playlist.video_urls)
+            for v in playlist.video_urls:
+                video.append(YouTube(v))
+        else:
+            video = YouTube(url)
     except:
         print("Couldn't find video at url:", url)
 
     if video is not None:
         # if its a playlist
         if str(type(video)) == "<class 'list'>":
+            print("Videos :", len(video))
             for v in video:
-                print("Title  : ", v.title)
-                print("Length : ", convert_time(int(v.length)))
-                print("Channel: ", v.author)
+                print("Title  :", v.title)
+                print("Length :", convert_time(int(v.length)))
+                print("Channel:", v.author)
                 print()
         # if its just a video
         else:
@@ -90,31 +114,32 @@ while inUse:
             print("Channel: ", video.author)
             print()
 
-            for s in video.streams.all():
+            # Uncomment this to see all stream types for a given video
+            '''
+            for s in video.streams:
                 print(s)
+            '''
 
             if input("Download this video (Y or N): ").lower() == 'y':
-                dl = input("Download mp4 or mp3: ")
-                while dl.lower() != 'mp4' and dl.lower() != 'mp3':
-                    dl = input("Enter mp3 or mp4 as download options: ")
+                dl = input("Download video or audio: ")
+                while dl.lower() != 'video' and dl.lower() != 'audio':
+                    dl = input("Enter video or audio as download options: ")
                     print()
-                if dl.lower() == 'mp4':
+                if dl.lower() == 'video':
                     res = input("Enter desired resolution (or high for highest avaliable): ")
                     if res.lower() == 'high':
                         stream = video.streams.get_highest_resolution()
                     else:
                         stream = video.stream.get_by_resolution(res)
 
-                elif dl.lower() == 'mp3':
+                elif dl.lower() == 'audio':
                     stream = video.streams.get_by_itag(audio_itag)
 
                 if stream is None:
-                    print("Error: Unable to find given stream quality or resolution")
+                    print("Error: Unable to find given stream quality or resolution (make sure resolution is in the form xxxxp, example 1080p)")
                 else:
                     print("Downloading...")
                     stream.download(output_path=download_dir, filename=video.title)
                     print("Download complete at", download_dir)
 
-    if input("Download another? (Y or N): ").lower() == 'n':
-        inUse = False
     print()
